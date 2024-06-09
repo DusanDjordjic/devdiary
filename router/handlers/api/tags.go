9d303ae3d71b578d9261e3fadf7c916649bbec2b
DusanDjordjic <dukidjordjic@gmail.com>
2024-06-09 15:45:22 +0200

    create tags handlers

--- router/handlers/api/tags.go
+++ router/handlers/api/tags.go
@@ -2,13 +2,26 @@ package api
 
 import (
 	"dev-diary/logger"
+	"dev-diary/models"
+	"dev-diary/router/web"
+	"dev-diary/services"
+	"net/http"
 
 	"github.com/labstack/echo"
+	"go.uber.org/zap"
 )
 
 func GetAllTagsHandler(c echo.Context) error {
 	log := logger.Log.Named("[GetAllTagsHandler]")
 	log.Info("started")
+
+	tags, total, err := services.GetAllTags()
+	if err != nil {
+		log.Error("failed to get tags", zap.Error(err))
+		return web.Error(http.StatusBadRequest, err)
+	}
+
+	web.DataWithCount(c, models.DTOTags(tags), total)
 	log.Info("finished")
 	return nil
 }
@@ -16,6 +29,20 @@ func GetAllTagsHandler(c echo.Context) error {
 func GetTagByIDHandler(c echo.Context) error {
 	log := logger.Log.Named("[GetTagByIDHandler]")
 	log.Info("started")
+
+	tagID, err := web.ParseID(c)
+	if err != nil {
+		log.Error("failed to parse id")
+		return err
+	}
+
+	tag, err := services.GetTagByID(tagID)
+	if err != nil {
+		log.Error("failed to get tag by id", zap.Error(err))
+		return web.Error(http.StatusNotFound, err)
+	}
+
+	web.Data(c, tag)
 	log.Info("finished")
 	return nil
 }
@@ -23,12 +50,27 @@ func GetTagByIDHandler(c echo.Context) error {
 func CreateTagHandler(c echo.Context) error {
 	log := logger.Log.Named("[CreateTagHandler]")
 	log.Info("started")
+
+	var payload services.CreateTagData
+	err := c.Bind(&payload)
+	if err != nil {
+		log.Error("failed to parse body", zap.Error(err))
+		return web.Error(http.StatusBadRequest, err)
+	}
+
+	tag, err := services.CreateTag(payload)
+	if err != nil {
+		log.Error("failed to create tag", zap.Error(err))
+		return web.InternalServerError()
+	}
+
+	web.Data(c, tag)
 	log.Info("finished")
 	return nil
 }
 
 func UpdateTagByIDHandler(c echo.Context) error {
-	log := logger.Log.Named("[UpdateTagByIDHandler]")
+	log := logger.Log.Named("[UpdatePostByIDHandler]")
 	log.Info("started")
 	log.Info("finished")
 	return nil
@@ -37,6 +79,20 @@ func UpdateTagByIDHandler(c echo.Context) error {
 func DeleteTagByIDHandler(c echo.Context) error {
 	log := logger.Log.Named("[DeleteTagByIDHandler]")
 	log.Info("started")
+
+	tagID, err := web.ParseID(c)
+	if err != nil {
+		log.Error("failed to parse id")
+		return err
+	}
+
+	err = services.DeleteTag(tagID)
+	if err != nil {
+		log.Error("failed to delete tag", zap.Error(err))
+		return web.Error(http.StatusBadRequest, err)
+	}
+
+	web.NoContent(c, http.StatusOK)
 	log.Info("finished")
 	return nil
 }
---
+++ services/tag.go
@@ -0,0 +1,48 @@
+package services
+
+import (
+	"dev-diary/db"
+	"dev-diary/models"
+)
+
+func GetAllTags() ([]models.Tag, int64, error) {
+	var tags []models.Tag
+	err := db.DB.Find(&tags).Error
+	if err != nil {
+		return nil, 0, err
+	}
+
+	var total int64
+	err = db.DB.Model(models.Tag{}).Count(&total).Error
+	if err != nil {
+		return nil, 0, err
+	}
+
+	return tags, total, nil
+}
+
+func GetTagByID(id uint) (models.Tag, error) {
+	var tag models.Tag
+	err := db.DB.Where("id = ?", id).First(&tag).Error
+	return tag, err
+}
+
+type CreateTagData struct {
+	Color           string `json:"color"`
+	BackgroundColor string `json:"background_color"`
+}
+
+func CreateTag(data CreateTagData) (models.Tag, error) {
+	tag := models.Tag{
+		Color:           data.Color,
+		BackgroundColor: data.BackgroundColor,
+	}
+
+	err := db.DB.Create(&tag).Error
+	return tag, err
+}
+
+func DeleteTag(id uint) error {
+	tag := models.Tag{ID: id}
+	return db.DB.Delete(&tag).Error
+}
